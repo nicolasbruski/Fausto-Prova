c2d9dc029d5ae26a65b28407844aa98b00c44c9e
RichardPFernandes <fernandesrichard312@gmail.com>
2024-04-11 21:11:32 -0300

    feat atualizacao

--- app-front/src/components/CardFarmacia.jsx
+++ app-front/src/components/CardFarmacia.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
+import '../styles/components/cardFarmacia.css'
 
 export default function CardFarmacia(props) {
     return (

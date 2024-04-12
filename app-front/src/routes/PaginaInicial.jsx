144936b131854026610fc56ab15587de00ee88bc
RichardPFernandes <fernandesrichard312@gmail.com>
2024-04-11 21:03:22 -0300

    feat styles

--- app-front/src/routes/PaginaInicial.jsx
+++ app-front/src/routes/PaginaInicial.jsx
@@ -1,6 +1,16 @@
+import React from 'react';
+import '../styles/PaginaInicial.css';
+import CardFarmacia from '../components/CardFarmacia';
 
-export default function PaginaInicial(){
-    return(
-        <div>HELLO WORLD</div>
+export default function PaginaInicial() {
+    return (
+        <div className="paginaInicial">
+            <div className="farmacia">
+                <h1>Principais Farmácias</h1>
+                <div className="farmacias">
+                    <CardFarmacia />
+                </div>
+            </div>
+        </div>
     )
 }

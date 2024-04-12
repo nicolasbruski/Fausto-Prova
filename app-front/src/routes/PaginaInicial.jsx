43b9405b04ddde642337a8f03736bdb2d244ee37
RichardPFernandes <fernandesrichard312@gmail.com>
2024-04-11 21:57:10 -0300

    correcao

--- app-front/src/routes/PaginaInicial.jsx
+++ app-front/src/routes/PaginaInicial.jsx
@@ -2,19 +2,42 @@ import React from 'react';
 import '../styles/PaginaInicial.css';
 import CardFarmacia from '../components/CardFarmacia';
 import catarinense from '../assets/catarinense.png';
+import nissei from '../assets/nissei.png';
+import panvel from '../assets/panvel.png';
+import precoPopular from '../assets/precoPopular.png';
+import pagueMenos from '../assets/pagueMenos.png';
+import { Button } from 'react-bootstrap';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
 
 export default function PaginaInicial() {
     return (
         <div className="paginaInicial">
-            <div className="farmacia">
+            <div className="farmacia conteudo">
                 <h1>Principais Farmácias</h1>
                 <div className="farmacias">
                     <CardFarmacia imagem={catarinense} nome="Catarinense"/>
-                    <CardFarmacia />
-                    <CardFarmacia />
-                    <CardFarmacia />
+                    <CardFarmacia imagem={nissei} nome="Nissei"/>
+                    <CardFarmacia imagem={panvel} nome="Panvel"/>
+                    <CardFarmacia imagem={precoPopular} nome="Preço Popular"/>
+                    <CardFarmacia imagem={pagueMenos} nome="Pague Menos"/>
                 </div>
             </div>
+            <div className="medicamentos conteudo">
+                <h1>Os Mais Buscados</h1>
+                <div className="medicamentos">
+                    <div className="button-right">
+                        {/*make icon left arrow with material ui/*/}
+                        <Button variant="light"><ChevronLeftIcon/></Button>
+                    </div>
+                    <div className="medicamento">
+                    </div>
+                    <div className="button-left">
+                        <Button variant="light"><ChevronRightIcon/></Button>
+                    </div>
+                </div>
         </div>
+    </div>
     )
 }
